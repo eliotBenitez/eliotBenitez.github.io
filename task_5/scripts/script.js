13fa3bb1c9e678ea003f81041d8ce1cca3ffb5cd
eliotBenitez <eliot.benitezhvat@gmail.com>
2023-10-06 14:26:00 +0300

    изменено:      task_5/scripts/script.js

--- task_5/scripts/script.js
+++ task_5/scripts/script.js
@@ -1,19 +1,19 @@
-var list = {
+let list = {
     1: 120,
     2: 64,
     3: 89,
     4: 349,
     5: 89,
-}
+};
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM loaded and parsed")
+    console.log("DOM loaded and parsed");
 });
 
 function buttonClicked() {
-    product = document.getElementById("selectItem");
-    count = document.getElementById("inputCount");
-    display = document.getElementById("calc-screen")
+    let product = document.getElementById('selectItem');
+    let count = document.getElementById('inputCount');
+    let display = document.getElementById('calc-screen');
 
     display.value = list[parseInt(product.value)] * parseInt(count.value);
-}
+};
