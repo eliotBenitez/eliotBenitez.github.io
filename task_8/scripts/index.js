aaadb66273d44575eaabd404be337b9271aa9e3a
eliotBenitez <eliot.benitezhvat@gmail.com>
2023-11-17 19:09:42 +0300

    new file:   "task_8/files/\320\237\320\276\320\273\320\270\321\202\320\270\320\272\320\260 \320\272\320\276\320\275\321\204\320\270\320\264\320\265\320\275\321\206\320\270\320\260\320\273\321\214\320\275\320\276\321\201\321\202\320\270.pdf" 	new file:   task_8/images/favicon.ico 	new file:   task_8/images/logo.png 	new file:   task_8/index.html 	new file:   task_8/scripts/index.js 	new file:   task_8/style.css

---
+++ task_8/scripts/index.js
@@ -0,0 +1,88 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const buttonForm = document.getElementById('buttonForm');
+    const popUp = document.getElementById('pop-up');
+    const form = document.getElementById('form');
+    const name = document.getElementById('inputName');
+    const email = document.getElementById('inputEmail');
+    const number = document.getElementById('inputNumber');
+    const organisation = document.getElementById('inputOrg');
+    const message = document.getElementById('inputMessage');
+    const btnSuccess = document.getElementById('btnSuccess');
+    const formList = [
+        [name, 'name'],
+        [email, 'email'],
+        [number, 'number'],
+        [organisation, 'organisation'],
+        [message, 'message']
+    ];
+
+    async function post(user) {
+        try {
+            let response = await fetch('https://formcarry.com/s/cG3t9oWv3d', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8' 
+                },
+                body: JSON.stringify(user)
+            });
+
+            let result = await response.json();
+            alert(result.message);
+
+        } catch (e) {
+            alert("Вы неправильно указали данные!");
+        }
+    }
+
+    buttonForm.addEventListener('click', () => {
+        popUp.style.display = 'flex';
+        buttonForm.style.display = 'none';
+        history.pushState(
+            {form: 'form'},
+            'form',
+            '#form.html'
+        );
+    });
+
+    popUp.addEventListener('click', () => {
+        history.back();
+        popUp.style.display = 'none';
+        buttonForm.style.display = 'flex';
+    });
+
+    form.addEventListener('click', (e) => {
+        e.stopPropagation();
+    });
+
+    formList.forEach((e) => {
+        e[0].value = localStorage.getItem(e[1]);
+        e[0].addEventListener('change', () => {
+            localStorage.setItem(e[1], e[0].value);
+        });
+    });
+
+    btnSuccess.addEventListener('click', async (e) => {
+        e.preventDefault();
+        
+        history.back();
+        popUp.style.display = 'none';
+        buttonForm.style.display = 'flex';
+
+        const user = {
+            name: name.value,
+            email: email.value,
+            number: number.value,
+            organisation: organisation.value,
+            message: message.value
+        };
+
+        console.log(user);
+
+        formList.forEach(e => {
+            localStorage.setItem(e[1], "");
+            e[0].value = "";
+        });
+
+        await post(user);
+    });
+})
